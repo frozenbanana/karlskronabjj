1414e9b70f7fcf4117517d93e02967f00b7442fd
frozenbanana <henrybergstrom@protonmail.com>
2021-12-20 09:01:17 +0100

    shortening qa text

--- pages/questions.js
+++ pages/questions.js
@@ -3,12 +3,12 @@ import { Container, Box, Text, Flex } from "@chakra-ui/layout";
 const questionsData = [
     {
         question: 'Behöver man särskilda kläder?',
-        answer: "Nejdå! Kom med vanliga gymkläder. Det viktigaste är att det inte finns dragkedjor eller annat som kan skada dig, andra eller mattan under sparring. Om du bestämmer dig för att fortsätta långsiktigt bör du införskaffa en GI. Det hjälper vi dig gärna med."
+        answer: "Nejdå! Kom med vanliga gymkläder. Det viktigaste är att det inte finns dragkedjor eller annat som kan skada dig, andra eller mattan under sparring."
     },
 
     {
         question: 'Kan jag börja mitt i en termin som nybörjare?',
-        answer: "Absolut! Det är bara att komma. När det finns flera nya på passet delar vi upp teknikdelen av träningen så att ni nya får en passade introduktion samtidigt som mer erfarna kan fokusera på sitt."
+        answer: "Absolut! Det är bara att komma. När det finns flera nya på passet delar vi upp teknikdelen av träningen så att ni nya får en passade introduktion."
     },
 
     {
