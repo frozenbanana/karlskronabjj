2e4a401acd9c28e321e30d70a84cf0f66c50be81
Henry Bergström <henrybergstrom@protonmail.com>
2021-12-28 15:33:08 +0100

    new map coordinates

--- components/Map.js
+++ components/Map.js
@@ -1,24 +1,18 @@
 import React from "react";
 import styles from "../styles/Map.module.css";
 
-// Generated from
-const MapFrame = () => (
-      <div className={styles.mapOuter}>
-        <div className={styles.gmapCanvas}>
-          <iframe
+const MapFrame = () => {
+  return (
+    <div className={styles.mapOuter}>
+      <div className={styles.gmapCanvas}>
+        <iframe
           className={styles.mapFrame}
-          width="512" 
           id="gmap_canvas"
-          src="https://maps.google.com/maps?q=SALTSJ%C3%96BADSV%C3%84GEN%201C&t=&z=13&ie=UTF8&iwloc=&output=embed" 
-          frameBorder="0" 
-          scrolling="no" 
-          marginHeight="0" 
-          marginWidth="0"></iframe>
-        </div>
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.1700840663293!2d15.598044166866407!3d56.171452638179325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x642876f070b7621c!2zNTbCsDEwJzE3LjAiTiAxNcKwMzUnNTkuNiJF!5e0!3m2!1sen!2sse!4v1640701877790!5m2!1sen!2sse"
+          width="512"
+          loading="eager"></iframe>
       </div>
+    </div>
   );
-
-
+}
 export default MapFrame;
-
-
